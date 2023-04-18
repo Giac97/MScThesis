8e5c8b2a323f0d84bb70e4ce1eade17ef7b79363
Giac97 <giacomo.becatti@studenti.unimi.it>
2023-04-18 15:19:10 +0200

    Testing some analysis tools from ovito lib on python

---
+++ Simulations/LAMMPS/Deposit/test/analysis_test.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 18 11:37:23 2023
+
+@author: becat
+"""
+
+from ovito.io import import_file, export_file
+from ovito.modifiers import SpatialBinningModifier, ComputePropertyModifier, ConstructSurfaceModifier
+import numpy as np
+import matplotlib.pyplot as plt
+
+pipeline = import_file("./snapshots/firstdep.xyz", columns=["Particle Type", "Position.X", "Position.Y", "Position.Z"])
+
+pipeline.modifiers.append(ConstructSurfaceModifier(
+    method = ConstructSurfaceModifier.Method.AlphaShape,
+    radius = 2.9,
+    identify_regions = True))
+
+
+data = pipeline.compute()
+
+print("Agregate quantities:")
+print(f"Surface area: {data.attributes['ConstructSurfaceMesh.surface_area']}")
+print(f"Solid volume: {data.attributes['ConstructSurfaceMesh.filled_volume']}")
+print(f"Filled volume fraction: {data.attributes['ConstructSurfaceMesh.filled_fraction']}")
+
+f_frac_firstdep = data.attributes['ConstructSurfaceMesh.filled_fraction']
