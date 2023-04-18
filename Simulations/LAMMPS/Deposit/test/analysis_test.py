11ca13a85eabeb93ee29fe439ce94eaa626f86cb
Giac97 <giacomo.becatti@studenti.unimi.it>
2023-04-18 19:48:56 +0200

    Added test of single step deposition

--- Simulations/LAMMPS/Deposit/test/analysis_test.py
+++ Simulations/LAMMPS/Deposit/test/analysis_test.py
@@ -26,3 +26,40 @@ print(f"Solid volume: {data.attributes['ConstructSurfaceMesh.filled_volume']}")
 print(f"Filled volume fraction: {data.attributes['ConstructSurfaceMesh.filled_fraction']}")
 
 f_frac_firstdep = data.attributes['ConstructSurfaceMesh.filled_fraction']
+empty_frac_firstdep = data.attributes['ConstructSurfaceMesh.empty_fraction']
+
+pipeline3 = import_file("./snapshots/thirddep.xyz", columns=["Position.X", "Position.Y", "Position.Z"])
+
+pipeline3.modifiers.append(ConstructSurfaceModifier(
+    method = ConstructSurfaceModifier.Method.AlphaShape,
+    radius = 2.9,
+    identify_regions = True))
+
+
+data3 = pipeline3.compute()
+
+print("Agregate quantities:")
+print(f"Surface area: {data3.attributes['ConstructSurfaceMesh.surface_area']}")
+print(f"Solid volume: {data3.attributes['ConstructSurfaceMesh.filled_volume']}")
+print(f"Filled volume fraction: {data3.attributes['ConstructSurfaceMesh.filled_fraction']}")
+
+f_frac_thirddep = data3.attributes['ConstructSurfaceMesh.filled_fraction']
+empty_frac_thirddep = data3.attributes['ConstructSurfaceMesh.empty_fraction']
+
+pipeline4 = import_file("./snapshots/fourthdep.xyz", columns=["Position.X", "Position.Y", "Position.Z"])
+
+pipeline4.modifiers.append(ConstructSurfaceModifier(
+    method = ConstructSurfaceModifier.Method.AlphaShape,
+    radius = 2.9,
+    identify_regions = True))
+
+
+data4 = pipeline4.compute()
+
+print("Agregate quantities:")
+print(f"Surface area: {data4.attributes['ConstructSurfaceMesh.surface_area']}")
+print(f"Solid volume: {data4.attributes['ConstructSurfaceMesh.filled_volume']}")
+print(f"Filled volume fraction: {data4.attributes['ConstructSurfaceMesh.filled_fraction']}")
+
+f_frac_forthtdep = data4.attributes['ConstructSurfaceMesh.filled_fraction']
+empty_frac_forthdep = data4.attributes['ConstructSurfaceMesh.empty_fraction']
---
+++ Simulations/LAMMPS/Deposit/test2/README.md
@@ -0,0 +1,2 @@
+# Single deposit
+This folder contains inputs and results from a simulation of a deposit happening in a single step, nanoparticles are deposited on the substrate and then equilibrated
